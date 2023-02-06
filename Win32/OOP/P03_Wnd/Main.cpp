90c02d5fa8fb63b58f903703b117f477ce736dc6
Atiq Rahman <atiqcx@gmail.com>
2023-02-05 17:14:30 -0800

    P03_Wnd: make runnable - unreferenced vars - Unicode chars with TEXT() - explicit cast of size_t for `_tcslen` - indent updated to 2 spaces

--- Win32/OOP/P03_Wnd/Main.cpp
+++ Win32/OOP/P03_Wnd/Main.cpp
@@ -3,6 +3,7 @@
 //
 // Author : Atiq Rahman
 // Date   : 02-16-2010
+// Status : Builds and runs great, HourHand appears though
 //
 
 
@@ -16,101 +17,100 @@ template <class DERIVED_TYPE>
 class BaseWindow
 {
 public:
-    static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+  static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+  {
+    DERIVED_TYPE *pThis = NULL;
+
+    if (uMsg == WM_CREATE)
     {
-        DERIVED_TYPE *pThis = NULL;
-
-        if (uMsg == WM_CREATE)
-        {
-            CREATESTRUCT* pCreate = (CREATESTRUCT*)lParam;
-            pThis = (DERIVED_TYPE*)pCreate->lpCreateParams;
-            SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)pThis);
-        }
-        else
-        {
-            pThis = (DERIVED_TYPE*)GetWindowLongPtr(hwnd, GWLP_USERDATA);
-        }
-        if (pThis)
-        {
-            return pThis->HandleMessage(uMsg, wParam, lParam);
-        }
-        else
-        {
-            return DefWindowProc(hwnd, uMsg, wParam, lParam);
-        }
+      CREATESTRUCT* pCreate = (CREATESTRUCT*)lParam;
+      pThis = (DERIVED_TYPE*)pCreate->lpCreateParams;
+      SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)pThis);
     }
-
-    BaseWindow() : m_hwnd(NULL) { }
-
-    BOOL Create(
-        PCWSTR lpWindowName,
-        DWORD dwStyle,
-        DWORD dwExStyle = 0,
-        int x = CW_USEDEFAULT,
-        int y = CW_USEDEFAULT,
-        int nWidth = CW_USEDEFAULT,
-        int nHeight = CW_USEDEFAULT,
-        HWND hWndParent = 0,
-        HMENU hMenu = 0
-        )
+    else
     {
-        WNDCLASS wc = {0};
-
-        wc.lpfnWndProc   = DERIVED_TYPE::WindowProc;
-        wc.hInstance     = GetModuleHandle(NULL);
-        wc.lpszClassName = ClassName();
-
-        RegisterClass(&wc);
-
-        m_hwnd = CreateWindowEx(
-            dwExStyle, ClassName(), lpWindowName, dwStyle, x, y,
-            nWidth, nHeight, hWndParent, hMenu, GetModuleHandle(NULL), this
-            );
-
-        return (m_hwnd ? TRUE : FALSE);
+      pThis = (DERIVED_TYPE*)GetWindowLongPtr(hwnd, GWLP_USERDATA);
     }
+    if (pThis)
+    {
+      return pThis->HandleMessage(uMsg, wParam, lParam);
+    }
+    else
+    {
+      return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+  }
 
-    HWND Window() const { return m_hwnd; }
+  BaseWindow() : m_hwnd(NULL) { }
+
+  BOOL Create(
+    PCWSTR lpWindowName,
+    DWORD dwStyle,
+    DWORD dwExStyle = 0,
+    int x = CW_USEDEFAULT,
+    int y = CW_USEDEFAULT,
+    int nWidth = CW_USEDEFAULT,
+    int nHeight = CW_USEDEFAULT,
+    HWND hWndParent = 0,
+    HMENU hMenu = 0
+    )
+  {
+    WNDCLASS wc = {0};
+
+    wc.lpfnWndProc   = DERIVED_TYPE::WindowProc;
+    wc.hInstance   = GetModuleHandle(NULL);
+    wc.lpszClassName = ClassName();
+
+    RegisterClass(&wc);
+
+    m_hwnd = CreateWindowEx(
+      dwExStyle, ClassName(), lpWindowName, dwStyle, x, y,
+      nWidth, nHeight, hWndParent, hMenu, GetModuleHandle(NULL), this
+      );
+
+    return (m_hwnd ? TRUE : FALSE);
+  }
+
+  HWND Window() const { return m_hwnd; }
 
 protected:
 
-    virtual PCWSTR  ClassName() const = 0;
-    virtual LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam) = 0;
+  virtual PCWSTR  ClassName() const = 0;
+  virtual LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam) = 0;
 
-    HWND m_hwnd;
+  HWND m_hwnd;
 };
 
 
 class MainWindow : public BaseWindow<MainWindow>
 {
 public:
-    PCWSTR  ClassName() const { return L"SA Window Class"; }
-    LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam);
+  PCWSTR  ClassName() const { return TEXT("Window Default Class"); }
+  LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam);
 };
 
 LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-    switch (uMsg)
+  switch (uMsg)
+  {
+  case WM_DESTROY:
+    PostQuitMessage(0);
+    return 0;
+
+  case WM_PAINT:
     {
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        return 0;
-
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(m_hwnd, &ps);
-            FillRect(hdc, &ps.rcPaint, (HBRUSH) (COLOR_WINDOW+1));
-			LPCWSTR TestStr = L"Saint Atique: Adopting Win32 with OOP";
-			TextOut(hdc, 25, 85, TestStr, _tcslen(TestStr));
-            EndPaint(m_hwnd, &ps);
-        }
-        return 0;
-
-    default:
-        return DefWindowProc(m_hwnd, uMsg, wParam, lParam);
+      PAINTSTRUCT ps;
+      HDC hdc = BeginPaint(m_hwnd, &ps);
+      FillRect(hdc, &ps.rcPaint, (HBRUSH) (COLOR_WINDOW+1));
+			LPCWSTR TestStr = L"OOP Base Class for Win32 App";
+			TextOut(hdc, 25, 85, TestStr, (int) _tcslen(TestStr));
+      EndPaint(m_hwnd, &ps);
     }
-    return TRUE;
+    return 0;
+
+  default:
+    return DefWindowProc(m_hwnd, uMsg, wParam, lParam);
+  }
 }
 
 //
@@ -122,23 +122,27 @@ LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
 //
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
 {
-    MainWindow win;
-
-    if (!win.Create(L"Windows OOP", WS_OVERLAPPEDWINDOW))
-    {
-        return 0;
-    }
-
-    ShowWindow(win.Window(), nCmdShow);
+  UNREFERENCED_PARAMETER(hInstance);
+  UNREFERENCED_PARAMETER(pCmdLine);
 
 
-    MSG msg = { };
-    // Windows message loop.
-    while (GetMessage(&msg, NULL, 0, 0))
-    {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
+  MainWindow win;
 
+  if (!win.Create(TEXT("Win32 OOP"), WS_OVERLAPPEDWINDOW))
+  {
     return 0;
+  }
+
+  ShowWindow(win.Window(), nCmdShow);
+
+
+  MSG msg = { };
+  // Windows message loop.
+  while (GetMessage(&msg, NULL, 0, 0))
+  {
+    TranslateMessage(&msg);
+    DispatchMessage(&msg);
+  }
+
+  return 0;
 }
